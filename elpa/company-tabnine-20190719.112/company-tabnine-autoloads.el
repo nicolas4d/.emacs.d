33ca9a44648184a81a35d39c2c43604c5ded90f8
nicolas4d <nicolas4d@foxmail.com>
2019-07-21 17:12:43 +0800

    rm .cache

--- elpa/company-tabnine-20190719.112/company-tabnine-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; company-tabnine-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "company-tabnine" "company-tabnine.el" (23857
-;;;;;;  53547 200741 507000))
-;;; Generated autoloads from company-tabnine.el
-
-(autoload 'company-tabnine "company-tabnine" "\
-`company-mode' backend for TabNine.
-
-See documentation of `company-backends' for details.
-
-\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; company-tabnine-autoloads.el ends here
--- elpa/gradle-mode-20150313.1205/gradle-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gradle-mode" "20150313.1205" "Gradle integration with Emacs' compile" '((s "1.8.0")) :commit "e4d665d5784ecda7ddfba015f07c69be3cfc45f2" :keywords '("gradle") :url "http://github.com/jacobono/emacs-gradle-mode")
