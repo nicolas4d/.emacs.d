161fc7dcb4d151e4417798750a1c128733749564
nicolas4d <nicolas4d@foxmail.com>
2019-08-15 11:41:14 +0800

    spacemacs rocks day 7

--- init-org.el
+++ init-org.el
@@ -20,13 +20,28 @@
 		      smartparens
 		      ;; --- Major Mode ---
 		      js2-mode
+		      web-mode
+		      evil
 		      ;; --- Minor Mode ---
 		      nodejs-repl
+		      js2-refactor
 		      ;;exec-path-from-shell
 		      ;; --- Themes ---
 		      monokai-theme
 		      ;; solarized-theme
 		      popwin
+		      ;;
+		      expand-region
+		      iedit
+		      org-pomodoro
+		      helm-ag
+		      flycheck
+		      auto-yasnippet
+		      evil-leader
+		      window-numbering
+		      evil-surround
+		      evil-nerd-commenter
+		      which-key
 		      ) "Default packages")
 
 (setq package-selected-packages my/packages)
@@ -42,6 +57,40 @@
   (dolist (pkg my/packages)
     (when (not (package-installed-p pkg))
       (package-install pkg))))
+
+;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
+(tool-bar-mode -1)
+
+;; 关闭文件滑动控件
+(scroll-bar-mode -1)
+
+;; 显示行号
+(global-linum-mode 1)
+
+;; 更改光标的样式（不能生效，解决方案见第二集）
+(setq-default cursor-type 'bar)
+
+;; 更改显示字体大小 16pt
+;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+;; too large
+(set-face-attribute 'default nil :height 110)
+
+;;最大化
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;;高亮当前行
+(global-hl-line-mode 1)
+
+;;monokai theme
+(load-theme 'monokai 1)
+
+;;yasnippet
+(require 'yasnippet)
+(yas-reload-all)
+(add-hook 'prog-mode-hook #'yas-minor-mode)
+
+;;flycheck
+(add-hook 'js2-mode-hook 'flycheck-mode)
 
 ;; smartparens
 (require 'smartparens-config)
@@ -78,12 +127,134 @@
 ;;选中的文字，编辑覆盖
 (delete-selection-mode 1)
 
-;;Highlight Matching Parenthesis
-(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
-
 ;; hungry-delete
 (require 'hungry-delete)
 (global-hungry-delete-mode)
+
+;; '' isuue
+(sp-local-pair '(emacs-lisp-mode lisp-interaction-mode) "'" nil :actions nil)
+
+;;js2-refactor
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+
+;;window-numbering
+(window-numbering-mode 1)
+
+;;after evil   
+;; evil-leader
+(global-evil-leader-mode)
+
+(evil-leader/set-key
+  "ff" 'find-file
+  "bb" 'switch-to-buffer
+  "0"  'select-window-0
+  "1"  'select-window-1
+  "2"  'select-window-2
+  "3"  'select-window-3
+  "w/" 'split-window-right
+  "w-" 'split-window-below
+  ":"  'counsel-M-x
+  "wM" 'delete-other-windows
+  )
+
+;; evil
+(evil-mode 1)
+(setcdr evil-insert-state-map nil)
+(define-key evil-insert-state-map [escape] 'evil-normal-state)
+
+;;evil-surround
+(require 'evil-surround)
+(global-evil-surround-mode)
+
+;;evil nerd commenter
+(evilnc-default-hotkeys)
+
+(define-key evil-normal-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
+(define-key evil-visual-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
+
+;;which-key
+(which-key-mode 1)
+
+(defun js2-imenu-make-index ()
+      (interactive)
+      (save-excursion
+	;; (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
+	(imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+				   ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+				   ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+				   ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+				   ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+				   ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
+				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+				   ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
+				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
+				   ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
+(add-hook 'js2-mode-hook
+	      (lambda ()
+		(setq imenu-create-index-function 'js2-imenu-make-index)))
+
+(defun occur-dwim ()
+  "Call `occur' with a sane default."
+  (interactive)
+  (push (if (region-active-p)
+	    (buffer-substring-no-properties
+	     (region-beginning)
+	     (region-end))
+	  (let ((sym (thing-at-point 'symbol)))
+	    (when (stringp sym)
+	      (regexp-quote sym))))
+	regexp-history)
+  (call-interactively 'occur))
+
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       '(("\\.html\\'" . web-mode))
+       auto-mode-alist))
+
+;;indent
+(defun my-toggle-web-indent ()
+  (interactive)
+  ;; web development
+  (if (or (eq major-mode 'js-mode) (eq major-mode 'js2-mode))
+      (progn
+	(setq js-indent-level (if (= js-indent-level 2) 4 2))
+	(setq js2-basic-offset (if (= js2-basic-offset 2) 4 2))))
+
+  (if (eq major-mode 'web-mode)
+      (progn (setq web-mode-markup-indent-offset (if (= web-mode-markup-indent-offset 2) 4 2))
+	     (setq web-mode-css-indent-offset (if (= web-mode-css-indent-offset 2) 4 2))
+	     (setq web-mode-code-indent-offset (if (= web-mode-code-indent-offset 2) 4 2))))
+  (if (eq major-mode 'css-mode)
+      (setq css-indent-offset (if (= css-indent-offset 2) 4 2)))
+
+  (setq indent-tabs-mode nil))
+
+;;首先下面是隐藏的方法:
+(defun hidden-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (unless buffer-display-table
+    (setq buffer-display-table (make-display-table)))
+  (aset buffer-display-table ?\^M []))
+
+;;以定义函数将此换行符删除，
+(defun remove-dos-eol ()
+  "Replace DOS eolns CR LF with Unix eolns CR"
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward "\r" nil t) (replace-match "")))
+
+;;Highlight Matching Parenthesis
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+(define-advice show-paren-function (:around (fn) fix-show-paren-function)
+  "Highlight enclosing parens."
+  (cond ((looking-at-p "\\s(") (funcall fn))
+	(t (save-excursion
+	     (ignore-errors (backward-up-list))
+	     (funcall fn)))))
 
 (require 'recentf)
 (recentf-mode 1)
@@ -169,8 +340,70 @@
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
+(require 'org)
+(setq org-src-fontify-natively t)
+
+;; 设置默认 Org Agenda 文件目录
+(setq org-agenda-files '("~/.emacs.d/org"))
+
+;; 设置 org-agenda 打开快捷键
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+;;capture templates
+(setq org-capture-templates
+	'(("t" "Todo" entry (file+headline "~/.emacs.d/gtd.org" "工作安排")
+	   "* TODO [#B] %?\n  %i\n"
+	   :empty-lines 1)))
+
+;;capture
+(global-set-key (kbd "C-c r") 'org-capture)
+
+;;org-pomodoro
+(set-language-environment "UTF-8")
+
+(with-eval-after-load 'org
+  ;; Org 模式相关设定
+  (require 'org-pomodoro)
+  )
+
+;;backward kill
+(global-set-key (kbd "C-w") 'backward-kill-word)
+
+;;auto-yasnippet
+(global-set-key (kbd "C-c d w") #'aya-create)
+(global-set-key (kbd "C-c d y") #'aya-expand)
+
+;;helm-ag
+(global-unset-key (kbd "C-c p"))
+(global-set-key (kbd "C-c p s") 'helm-do-ag-project-root)
+
+;;comanymode
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
+;;iedit
+(global-set-key (kbd "M-s e") 'iedit-mode)
+
+;;expand-region
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;;imenu
+(global-set-key (kbd "M-s i") 'counsel-imenu)
+
+;;occur
+(global-set-key (kbd "M-s o") 'occur-dwim)
+
+;;js2-refator
+(js2r-add-keybindings-with-prefix "C-c C-m")
+
+;;web-mode indent
+(global-set-key (kbd "C-c t i") 'my-toggle-web-indent)
+
 ;;fast to open init file
-(global-set-key (kbd "<f3>") 'open-init-file)
+(global-set-key (kbd "<f5>") 'open-init-file)
 
 ;; recentf
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
@@ -197,38 +430,3 @@
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-
-;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
-(tool-bar-mode -1)
-
-;; 关闭文件滑动控件
-(scroll-bar-mode -1)
-
-;; 显示行号
-(global-linum-mode 1)
-
-;; 更改光标的样式（不能生效，解决方案见第二集）
-(setq-default cursor-type 'bar)
-
-;; 更改显示字体大小 16pt
-;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
-;; too large
-(set-face-attribute 'default nil :height 110)
-
-;;最大化
-(setq initial-frame-alist (quote ((fullscreen . maximized))))
-
-;;高亮当前行
-(global-hl-line-mode 1)
-
-;;monokai theme
-(load-theme 'monokai 1)
-
-(require 'org)
-(setq org-src-fontify-natively t)
-
-;; 设置默认 Org Agenda 文件目录
-(setq org-agenda-files '("~/.emacs.d/org"))
-
-;; 设置 org-agenda 打开快捷键
-(global-set-key (kbd "C-c a") 'org-agenda)
--- init.el
+++ init.el
@@ -4,3 +4,15 @@
 (require 'ob-tangle)
 ;; org 文件名不可以是init.org
 (org-babel-load-file (expand-file-name "init-org.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-leader/leader "SPC"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
