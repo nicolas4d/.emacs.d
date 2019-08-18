7a3334d5dbb8fce9316904d5b09c8c2cf77dbd0a
nicolas4d <nicolas4d@foxmail.com>
2019-08-18 15:39:40 +0800

    for checkout

---
+++ elpa/27.0/highlight-global-20140125.1139/highlight-global-autoloads.el
@@ -0,0 +1,23 @@
+;;; highlight-global-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "highlight-global" "highlight-global.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from highlight-global.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "highlight-global" '("buffer-highlight-list" "check-whether-highlighted" "clear-" "find-and-use-face" "force-highlight-frame" "get-thing-to-highlight" "global-highlight-list" "highlight-" "new-" "release-face" "unhighlight-window" "update-highlight-fixup")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; highlight-global-autoloads.el ends here
