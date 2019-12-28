6219d144847f912a9838a6c2ba221f0fe2dbe316
nicolas4d <nicolas4d@foxmail.com>
2019-12-29 06:43:54 +0800

    disable dired one buffer

--- .spacemacs.d/layers/nicolas4d-base/config.el
+++ .spacemacs.d/layers/nicolas4d-base/config.el
@@ -14,14 +14,14 @@
 
 
 ;; 单个缓冲区
-(put 'dired-find-alternate-file 'disabled nil)
+;; (put 'dired-find-alternate-file 'disabled nil)
 
 ;; 主动加载 Dired Mode
 ;; (require 'dired)
 ;; (defined-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
 ;; 延迟加载
-(with-eval-after-load 'dired
-  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+;;(with-eval-after-load 'dired
+;;  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
 
 ;; 拷贝，两个窗口时自动选择另一个窗口
 (setq dired-dwin-target 1)
