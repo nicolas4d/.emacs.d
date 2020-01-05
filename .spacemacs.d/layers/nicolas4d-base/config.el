d15cbbd3f302e31347c31d2e4522d87076def227
nicolas4d <nicolas4d@foxmail.com>
2020-01-05 10:11:29 +0800

    dired less buffer (one buffer)

--- .spacemacs.d/layers/nicolas4d-base/config.el
+++ .spacemacs.d/layers/nicolas4d-base/config.el
@@ -12,18 +12,17 @@
 (setq dired-recursive-deletes 'always)
 (setq dired-recursive-copies 'always)
 
-
 ;; 单个缓冲区
-;; (put 'dired-find-alternate-file 'disabled nil)
+(put 'dired-find-alternate-file 'disabled nil)
 
 ;; 主动加载 Dired Mode
 ;; (require 'dired)
 ;; (defined-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
 ;; 延迟加载
-;;(with-eval-after-load 'dired
-;;  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+(with-eval-after-load 'dired
+  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
 
 ;; 拷贝，两个窗口时自动选择另一个窗口
-(setq dired-dwin-target 1)
+;;(setq dired-dwin-target 1)
 
 ;;; dired ends here
