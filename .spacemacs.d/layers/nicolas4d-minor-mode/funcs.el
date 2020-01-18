47208035364ed6d6e5af3057f770e22ccc6604fe
nicolas4d <nicolas4d@foxmail.com>
2020-01-18 09:35:19 +0800

    .cask

--- .spacemacs.d/layers/nicolas4d-minor-mode/funcs.el
+++ .spacemacs.d/layers/nicolas4d-minor-mode/funcs.el
@@ -6,7 +6,7 @@
 (defun nicolas4d/copy-picture-to-dir ()
   "copyt ~/Pictures/* to dir that I worked Now."
   (interactive)
-  (shell-command "mv ~/Pictures/* /home/d/DesignPatterns/GOF/")
+  (shell-command "mv ~/Pictures/* /home/d/DesignPatterns/DesignPatterns-GOF/")
   )
 
 (defun nicolas4d/exec-xmodmap ()
