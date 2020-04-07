c38503b4cd8340db54ce4cdc0ed24ffe6d3e908c
nicolas4d <nicolas4d@foxmail.com>
2020-04-08 01:28:50 +0800

    .spcameacs

--- .spacemacs.d/layers/nicolas4d-minor-mode/funcs.el
+++ .spacemacs.d/layers/nicolas4d-minor-mode/funcs.el
@@ -8,11 +8,12 @@
   (interactive)
   ;; (shell-command "mv ~/Pictures/* /home/d/DesignPatterns/DesignPatterns-GOF/img/")
   ;; (shell-command "mv ~/Pictures/* /home/d/python/Problem-Solving-with-Algorithms-and-Data-Structures-using-Python/")
-  (shell-command "mv ~/Pictures/* /home/d/android/Head-First-Android-Development/img/")
+  ;; (shell-command "mv ~/Pictures/* /home/d/android/Head-First-Android-Development/img/")
   ;; (shell-command "mv ~/Pictures/* /home/d/python/PyWebScraping/img/")
   ;; (shell-command "mv ~/Pictures/* /home/d/python/Scrapy/img/")
   ;; (shell-command "mv ~/Pictures/* /home/d/Chrome/Extentions/Chrome扩展及应用开发（首发版）/img/")
   ;; (shell-command "mv ~/Pictures/* /home/d/.emacs.d/note/yasnippet/img/")
+  (shell-command "mv ~/Pictures/* /home/d/android/docs/guide/img/")
   )
 
 (defun nicolas4d/exec-xmodmap ()
--- .spacemacs.d/layers/nicolas4d/keybindings.el
+++ .spacemacs.d/layers/nicolas4d/keybindings.el
@@ -1,17 +1,17 @@
 (global-set-key (kbd "M-s") 'save-buffer)
 (global-set-key (kbd "M-m") 'set-mark-command)
 (global-set-key (kbd "C-<tab>") 'spacemacs/alternate-buffer)
-
 (global-set-key (kbd "M-x") nil)
 (global-set-key (kbd "M-x") 'kmacro-call-macro)
-
 (global-set-key (kbd "C-M-k") 'kill-paragraph)
 (global-set-key (kbd "C-j") 'end-of-line-and-new-line-and-indent)
 (global-unset-key (kbd "C-<SPC>"))
-
 (keyboard-translate ?\C-h ?\C-?)
 
 (spacemacs/set-leader-keys
+  ;; layout
+  "<tab>" 'spacemacs/jump-to-last-layout
+
   ;; kmacro
   "ms" 'kmacro-start-macro
   "me" 'kmacro-end-macro
