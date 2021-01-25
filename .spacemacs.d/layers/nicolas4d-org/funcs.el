e53e21f274187ec56c682b14fe3149d7cd5bf584
d <nicolas4d@foxmail.com>
2021-01-25 23:19:08 +0800

    fix bug img-copy.

--- .spacemacs.d/layers/nicolas4d-org/funcs.el
+++ .spacemacs.d/layers/nicolas4d-org/funcs.el
@@ -11,6 +11,9 @@
 
 (defvar nicolas4d/notify-picture-dir-add-file-descriptor nil)
 
+(defvar nicolas4d/notify-bind-buffer nil
+  "Used to find buffer.")
+
 (defun nicolas4d/toggle-notify-picture-dir-add-file()
   "Notify ~/Pictures dir add event."
   (interactive)
@@ -29,27 +32,33 @@
 
 ;; (nicolas4d/toggle-notify-picture-dir-add-file)
 
-(defvar nicolas4d/notify-bind-buffer nil
-  "Used to find buffer.")
+(defun nicolas4d/advice-notify-picture-dir-add-file-close (&optional ARG)
+  "Advice when emacs close."
+  (when nicolas4d/notify-picture-dir-add-file-descriptor
+    (progn
+      (file-notify-rm-watch nicolas4d/notify-picture-dir-add-file-descriptor)
+      (setq nicolas4d/notify-picture-dir-add-file-descriptor nil)
+      (message "remove notify!"))))
 
-(defun nicolas4d/notify-picture-dir-add-file-sleep (event)
-  (when (or (string= (symbol-name (nth 1 event)) "created")
-            (string= (symbol-name (nth 1 event)) "changed"))
-    (sleep-for 0 100)
-    (nicolas4d/notify-picture-dir-add-file-sleep event)))
+(advice-add 'save-buffers-kill-terminal
+            :before
+            'nicolas4d/advice-notify-picture-dir-add-file-close)
 
 (defun nicolas4d/notify-picture-dir-add-file-callback (event)
   "callback"
-  (when (string= (symbol-name (nth 1 event)) "created")
-    (with-timeout (1
-                   (progn
-                     (if nicolas4d/notify-bind-buffer
-                         (purpose-select-buffer nicolas4d/notify-bind-buffer)
-                       (current-buffer))
-                     (end-of-buffer)
-                     (yas-expand-snippet (yas-lookup-snippet "Img source"))))
-      (print event)
-      (nicolas4d/notify-picture-dir-add-file-sleep event))))
+  (when (string= (symbol-name (nth 1 event)) "changed")
+    (let* ((cur-img-attr (file-attributes (nth 2 event)))
+           (cur-img-size (file-attribute-size cur-img-attr))
+           )
+      (sleep-for 0 200)
+      (if (/= cur-img-size (file-attribute-size (file-attributes (nth 2 event))))
+          (message "img size chage!")
+        (progn
+          (message "img size not change")
+          (if nicolas4d/notify-bind-buffer 
+              (purpose-select-buffer nicolas4d/notify-bind-buffer)
+            (current-buffer))
+          (yas-expand-snippet (yas-lookup-snippet "Img source")))))))
 
 (defun nicolas4d/toggle-notify-bind-buffer ()
   (interactive)
