970c674b5cd8886e00efe8891e6552aa38fb6eb5
d <nicolas4d@foxmail.com>
2021-02-06 21:34:55 +0800

    Removme abbrev for nicolas4d.

--- .spacemacs.d/layers/nicolas4d-base/config.el
+++ .spacemacs.d/layers/nicolas4d-base/config.el
@@ -1,7 +1,7 @@
 ;;; abbrev
 (setq-default abbrev-mode t)
 
-(define-abbrev-table 'global-abbrev-table '(
-                                            ("n4d" "nicolas4d")
-                                            ))
+;; (define-abbrev-table 'global-abbrev-table '(
+;;                                             ("n4d" "nicolas4d")
+;;                                             ))
 ;;; abbrev ends here
